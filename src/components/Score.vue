20a435e8f05dbad1014b78d82aefb5c8e33cc00f
ryosuke1217 <ryosuke121720080711@gmail.com>
2018-05-09 17:07:45 +0900

    rod追加

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
+    <img src="../assets/rod.png" alt="棒線" title="棒">
     <div id="container"></div>
   </div>
 </template>
 <script src="osmd.js"></script>
 <script>
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay'
+import { Parser } from 'xml-parser'
 
 window.onload = () => {
   let osmd = new OpenSheetMusicDisplay(document.getElementById('container'))
-  osmd.load('http://localhost:8888/kaerunouta.xml').then(_ => osmd.render())
+  osmd.load('http://localhost:8888/kaerunouta.xml')
+  .then(_ => osmd.render())
+  .then(_ => console.log(osmd.drawer))
+  .catch(e => console.log(e))
 }
 
 export default {
@@ -21,5 +26,12 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+
+img {
+  position: absolute;
+  left: 110px;
+  top: 190px;
+  z-index: 1;
 }
 </style>
