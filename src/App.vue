213e4a8039e307eb93f5084c36fb38771954a55f
ishiguro <yuu.ishiguro@gmail.com>
2018-05-13 17:43:50 +0900

    add reset scss

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,8 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import url("../static/scss/reset.scss");
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
